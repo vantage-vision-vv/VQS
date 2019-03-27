2d555c9a7865f7a3d7572dede737afa2bd0a77e8
abhay97ps <abhay97ps@gmail.com>
2019-03-27 15:32:02 +0530

    update

--- ARL/videolstm.py
+++ ARL/videolstm.py
@@ -26,6 +26,9 @@ class videolstm(object):
         self.feature_out = 512
         self.ctx_in = 512
         self.ctx_out = 512
+        self.atten_kernel = (3,3,512,512)
+        self.atten_transition = (1,1,512,1)
+        self.atten_bias = (1,7,7,512)
 
         self.actions = 51  # HMDB51
         self.timesteps = 30
@@ -117,18 +120,13 @@ class videolstm(object):
             "b_second_c", shape=self.feature_out, initializer=tf.constant_initializer(value=0))
 
         # attention cnn
-        self.W_inter_xa = tf.get_variable("W_inter_xa", shape=(
-            self.feature_in, self.feature_in), initializer=tf.contrib.layers.xavier_initializer())
-        self.W_inter_ha = tf.get_variable("W_inter_ha", shape=(
-            self.ctx_in, self.feature_in), initializer=tf.contrib.layers.xavier_initializer())
-        self.W_inter_z = tf.get_variable("W_inter_z", shape=(
-            self.feature_in, 1), initializer=tf.contrib.layers.xavier_initializer())
-        self.b_inter_a = tf.get_variable("b_inter_a", shape=(
-            self.feature_in,), initializer=tf.constant_initializer(value=0))
+        self.W_inter_xa = tf.get_variable("W_inter_xa", shape=self.atten_kernel, initializer=tf.contrib.layers.xavier_initializer())
+        self.W_inter_ha = tf.get_variable("W_inter_ha", shape=self.atten_kernel, initializer=tf.contrib.layers.xavier_initializer())
+        self.W_inter_z = tf.get_variable("W_inter_z", shape=self.atten_transition, initializer=tf.contrib.layers.xavier_initializer())
+        self.b_inter_a = tf.get_variable("b_inter_a", shape=self.atten_bias, initializer=tf.constant_initializer(value=0))
 
     def clstm_forward(self, prev, inp):
-        H_first_tm1, C_first_tm1, H_second_tm1, C_second_tm1, _ = tf.unstack(
-            prev)
+        H_first_tm1, C_first_tm1, H_second_tm1, C_second_tm1, _ = prev  
         M_t = inp[:, :, :512]
         X_t = inp[:, :, 512:]
         M_t = tf.reshape(M_t, (1, 7, 7, 512))
@@ -138,37 +136,43 @@ class videolstm(object):
             tf.add(
                 tf.add(
                     tf.add(
-                        conv(M_t, self.W_first_xi), conv(H_first_tm1, self.W_first_hi)),
-                    conv(H_second_tm1, self.W_first_ei)),
+                        conv(M_t, self.W_first_xi, padding='SAME'), conv(H_first_tm1, self.W_first_hi, padding='SAME')),
+                    conv(H_second_tm1, self.W_first_ei, padding='SAME')),
                 self.b_first_i))
         F_first_t = sig(
             tf.add(
                 tf.add(
                     tf.add(
-                        conv(M_t, self.W_first_xf), conv(H_first_tm1, self.W_first_hf)),
-                    conv(H_second_tm1, self.W_first_ef)),
+                        conv(M_t, self.W_first_xf, padding='SAME'), conv(H_first_tm1, self.W_first_hf, padding='SAME')),
+                    conv(H_second_tm1, self.W_first_ef, padding='SAME')),
                 self.b_first_f))
         O_first_t = sig(
             tf.add(
                 tf.add(
                     tf.add(
-                        conv(M_t, self.W_first_xo), conv(H_first_tm1, self.W_first_ho)),
-                    conv(H_second_tm1, self.W_first_eo)),
+                        conv(M_t, self.W_first_xo, padding='SAME'), conv(H_first_tm1, self.W_first_ho, padding='SAME')),
+                    conv(H_second_tm1, self.W_first_eo, padding='SAME')),
                 self.b_first_o))
         G_first_t = tanh(
             tf.add(
                 tf.add(
                     tf.add(
-                        conv(M_t, self.W_first_xc), conv(H_first_tm1, self.W_first_hc)),
-                    conv(H_second_tm1, self.W_first_ec)),
+                        conv(M_t, self.W_first_xc, padding='SAME'), conv(H_first_tm1, self.W_first_hc, padding='SAME')),
+                    conv(H_second_tm1, self.W_first_ec, padding='SAME')),
                 self.b_first_c))
         C_first_t = tf.add(mul(F_first_t, C_first_tm1),
                            mul(I_first_t, G_first_t))
         H_first_t = mul(O_first_t, tanh(C_first_t))
 
         # intermediate attention cnn layer
-        Z_t = conv(self.W_inter_z, tanh(
-            tf.add(tf.add(conv(X_t, self.W_inter_xa), conv(H_first_t, self.W_inter_ha)), self.b_inter_a)))
+        Z_t = conv(
+            tanh(
+                tf.add(
+                    tf.add(
+                        conv(X_t, self.W_inter_xa, padding='SAME'),
+                        conv(H_first_t, self.W_inter_ha, padding='SAME')),
+                    self.b_inter_a)),
+            self.W_inter_z, padding='SAME')
         A_t = exp(Z_t)/np.sum(exp(Z_t))
         X_tilda_t = mul(A_t, X_t)
 
@@ -176,28 +180,28 @@ class videolstm(object):
         I_second_t = sig(
             tf.add(
                 tf.add(
-                    conv(X_tilda_t, self.W_second_xi), conv(H_second_tm1, self.W_second_hi)),
+                    conv(X_tilda_t, self.W_second_xi, padding='SAME'), conv(H_second_tm1, self.W_second_hi, padding='SAME')),
                 self.b_second_i))
         F_second_t = sig(
             tf.add(
                 tf.add(
-                    conv(X_tilda_t, self.W_second_xf), conv(H_second_tm1, self.W_second_hf)),
+                    conv(X_tilda_t, self.W_second_xf, padding='SAME'), conv(H_second_tm1, self.W_second_hf, padding='SAME')),
                 self.b_second_f))
         O_second_t = sig(
             tf.add(
                 tf.add(
-                    conv(X_tilda_t, self.W_second_xo), conv(H_second_tm1, self.W_second_ho)),
+                    conv(X_tilda_t, self.W_second_xo, padding='SAME'), conv(H_second_tm1, self.W_second_ho, padding='SAME')),
                 self.b_second_o))
         G_second_t = sig(
             tf.add(
                 tf.add(
-                    conv(X_tilda_t, self.W_second_xc), conv(H_second_tm1, self.W_second_hc)),
+                    conv(X_tilda_t, self.W_second_xc, padding='SAME'), conv(H_second_tm1, self.W_second_hc, padding='SAME')),
                 self.b_second_c))
         C_second_t = tf.add(mul(F_second_t, C_second_tm1),
                             mul(I_second_t, G_second_t))
         H_second_t = mul(O_second_t, tanh(C_second_t))
 
-        return tf.stack([H_first_t, C_first_t, H_second_t, C_second_t, A_t])
+        return [H_first_t, C_first_t, H_second_t, C_second_t, A_t]
 
     def forward(self, Z):
         M_t = Z[:, :, :, :512]
@@ -214,8 +218,9 @@ class videolstm(object):
         # CNN_4 initialization for second lstm hidden state
         cnn_4_output = conv(X_t[0:1], self.W_cnn_4, padding="SAME")
 
+        At_t = tf.get_variable("At_t", shape=(1,7,7,1), initializer=tf.constant_initializer(value=0))
         initial_states = [cnn_2_output, cnn_1_output,
-                          cnn_4_output, cnn_3_output, cnn_1_output]
+                          cnn_4_output, cnn_3_output, At_t]
         '''
         augmented_input = []
         for i in range(self.timesteps):
@@ -224,10 +229,11 @@ class videolstm(object):
         output_states = tf.scan(
             self.clstm_forward, Z, initializer=initial_states)
 
-        temp_1 = tf.layers.dense(output_states[2], 1024, activation="tanh")
+        fc_input = tf.reshape(output_states[2], [1,-1])
+        temp_1 = tf.layers.dense(fc_input, 1024, activation="tanh")
         temp_2 = tf.layers.dropout(temp_1, rate=0.7)
         out = tf.layers.dense(temp_2, self.actions, activation=None)
-
+        out = tf.reshape(out, [-1])
         return out
 
     def train(self):
@@ -255,6 +261,7 @@ class videolstm(object):
                 # training set
                 for i in range(len(train)):
                     Z_pass, y_pass = data.get_data(train[i])
+                    y_pass = tf.one_hot(y_pass,self.actions)
                     _, loss = sess.run([optimizer, cost], feed_dict={
                         self.Z: Z_pass,
                         self.y: y_pass
