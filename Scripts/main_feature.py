5eb5dfe05a1091df260ab2c929119b3ea93e9a25
abhay97ps <abhay97ps@gmail.com>
2019-04-10 11:44:04 +0530

    update

--- Scripts/main_feature.py
+++ Scripts/main_feature.py
@@ -2,6 +2,7 @@ import os
 from feature_extraction import extract_features
 from feature_selection import select_features
 import numpy as np
+from pathlib import Path
 
 
 data_dir = '/tmp/Virat/'
@@ -15,9 +16,15 @@ with open("/tmp/Data/virat_features/class_label.txt", "w") as fl:
 
 
 for index, item in enumerate(classes):
-    video_label, video_name = extract_features([item], index)
+    #video_label, video_name = extract_features([item], index)
+    video_name = np.array(
+        list(Path(data_dir).rglob("*.[a][v][i]"))).astype(str)
+    video_label = []
+    for i in range(video_name.shape[0]):
+        video_name[i] = video_name[i].split('/')[-1]
+        video_label.append(int(video_name[i].split('_')[0]))
+    select_features(video_label, video_name)
     '''
-    select_features(video_label,video_name)
     os.system(" rm -rf /tmp/Data/virat_features/data_file")
     os.system(" rm -rf /tmp/Data/virat_features/context_file")
     os.system("mkdir /tmp/Data/virat_features/data_file")
