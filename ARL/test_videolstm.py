c27866b79f1bea6d40b5714881f6e0f2e220f245
abhay97ps <abhay97ps@gmail.com>
2019-05-12 12:28:14 +0530

    testing vlstm on virat

--- ARL/test_videolstm.py
+++ ARL/test_videolstm.py
@@ -20,12 +20,12 @@ if __name__ == '__main__':
         #################################
         # load saved model
         saver = tf.train.import_meta_graph(
-            'Models/HMDB51/videolstm_model-42.meta')
-        saver.restore(sess, tf.train.latest_checkpoint('Models/HMDB51/'))
+            'Models/VIRAT/videolstm_model-42.meta')
+        saver.restore(sess, tf.train.latest_checkpoint('Models/VIRAT/'))
         graph = tf.get_default_graph()
         Z = graph.get_tensor_by_name('input:0')
         out = graph.get_tensor_by_name('output:0')
-        Att_t = graph.get_tensor_by_name('attention:0')
+        #Att_t = graph.get_tensor_by_name('attention:0')
         #################################
         # test data predictions
         for i in range(len(test)):
@@ -34,10 +34,10 @@ if __name__ == '__main__':
             except Exception:
                 continue
 
-            pred, att_map = sess.run([out, Att_t], feed_dict={Z: Z_pass})
+            pred = sess.run([out], feed_dict={Z: Z_pass})
 
             pred_test.append(np.argmax(pred))
-            y_test.append(y_target)
+            y_test.append(int(y_target)-1)
 
     report = classification_report(y_true=y_test, y_pred=pred_test)
     acc = accuracy_score(y_true=y_test, y_pred=pred_test)
