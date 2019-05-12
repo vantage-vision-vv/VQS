b9ab5efa2830dc35ecb773855a244582c99bba2c
abhay97ps <abhay97ps@gmail.com>
2019-05-12 13:52:25 +0530

    generate virat data for fcn

--- Scripts/virat_attention_map.py
+++ Scripts/virat_attention_map.py
@@ -1,10 +1,12 @@
 import numpy as np
 import tensorflow as tf
 import cv2
+import h5py
 
-vid_path = "/tmp/hmdb51/hmdb/"
-data_path = "/tmp/Data/hmdb_input/"
-map_path = "./Data/"
+vid_path = "/tmp/Virat_Trimed/"
+data_path = "/tmp/Data/virat_input/"
+map_path = "/Data/"
+bb_path = "/tmp/Data/virat_input/bb_file/"
 
 
 def run_vlstm(inp):
@@ -15,44 +17,61 @@ def run_vlstm(inp):
     return att_map
 
 
-def get_label_image(bb):
+def get_label_image(bb, shape):
     bb = list(map(float, bb))
     bb = list(map(int, bb))
-    label = np.zeros((224, 224))
+    label = np.zeros(shape)
     label[bb[0]:bb[2]+bb[0], bb[1]:bb[3]+bb[1]] = 1
-    return label
+    return cv2.resize(label, (224, 224))
 
 
 def store_attn(p):
     with open(map_path+"map_"+p+".txt", "r") as f:
         for cnt, line in enumerate(f):
             data = line.strip().split(" ")
-            input_data = np.load(data_path + p + "/" + data[0] + ".npy")
-            frame_data = list(map(int, data[1].split(",")))
             vid_name = "_".join(x for x in data[0].split("_")[:-1])
+            input_data = np.load(data_path + p + "/" + data[0] + ".npy")
+            #bb_data = np.load(bb_path + vid_name + ".h5")
+            bb_file = h5py.File(bb_path + vid_name + ".h5", 'r')
+            bb_data = np.array(bb_file.get('bb_file'))
+            bb_file.close()
+            frame_data = list(map(int, data[1].split(",")))
             cap = cv2.VideoCapture(vid_path + vid_name)
             length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
             img_data = []
             label_data = []
             _, _ = cap.read()
+
+            crop_cnt = 0
             for i in range(length-1):
                 ret, frame = cap.read()
                 if i in frame_data:
-                    gray = cv2.resize(frame, (224, 224))
-                    img_data.append(gray)
-                    label_data.append(get_label_image(
-                        input_data[2][len(img_data) - 1]))
+                    frame = frame[bb_data[i][2]:bb_data[i]
+                                  [3], bb_data[i][0]:bb_data[i][1]]
+                    origin_cropped = [bb_data[i][0], bb_data[i][2]]
+                    bb_original = input_data[2][crop_cnt]
+                    crop_cnt += 1
+                    bb_cropped = [bb_original[0]-origin_cropped[0], bb_original[1] -
+                                  origin_cropped[1], bb_original[2], bb_original[3]]
+
+                    target = get_label_image(bb_cropped, frame.shape)
+                    frame = cv2.resize(frame, (224, 224))
+                    img_data.append(frame)
+                    label_data.append(target)
+
             attn_data = run_vlstm(input_data[0])
-            np.savez(data_path + p + "_attn/" + str(cnt) + ".npy", np.array(img_data).reshape((10, 224, 224, 3)),
-                     np.array(attn_data).reshape((10, 7, 7, 1)), np.array(label_data).reshape((10, 224, 224, 1)))
+            np.savez(data_path + p + "_attn/" + str(cnt) + ".npy", np.array(img_data).reshape((30, 224, 224, 3)),
+                     np.array(attn_data).reshape((30, 7, 7, 1)), np.array(label_data).reshape((30, 224, 224, 1)))
 
 
-with tf.Session() as sess:
-    saver = tf.train.import_meta_graph('Models/VIRAT/videolstm_model-42.meta')
-    saver.restore(sess, tf.train.latest_checkpoint('Models/VIRAT/'))
-    graph = tf.get_default_graph()
-    Z = graph.get_tensor_by_name('input:0')
-    Att_t = graph.get_tensor_by_name('attention:0')
-    store_attn("train")
-    store_attn("val")
-    store_attn("test")
+if __name__ == "__main__":
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(
+            'Models/VIRAT/videolstm_model-42.meta')
+        saver.restore(sess, tf.train.latest_checkpoint('Models/VIRAT/'))
+        graph = tf.get_default_graph()
+        Z = graph.get_tensor_by_name('input:0')
+        Att_t = graph.get_tensor_by_name('attention:0')
+        store_attn("train")
+        store_attn("val")
+        store_attn("test")
